94a6709ddc25eea8431742758e4c39d66b052a96
Nick Heggø <nickhe@stud.ntnu.no>
2025-03-05 11:53:07 +0100

    feat(ui): add bottom panel with game stats display

--- src/main/java/dev/nheggoe/cardgame/frontend/JavaFXMain.java
+++ src/main/java/dev/nheggoe/cardgame/frontend/JavaFXMain.java
@@ -6,9 +6,11 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
@@ -55,15 +57,51 @@ public class JavaFXMain extends Application {
         rightPanel.setAlignment(Pos.TOP_CENTER);
 
         Button dealHandButton = new Button("Deal Hand");
-        dealHandButton.setOnAction(e -> {
-            dealCards();
-        });
+        dealHandButton.setOnAction(e -> dealCards());
 
         Button checkHandButton = new Button("Check Hand");
         checkHandButton.setOnAction(e -> checkHand());
 
         rightPanel.getChildren().addAll(dealHandButton, checkHandButton);
         root.setRight(rightPanel);
+
+        GridPane bottomPanel = new GridPane();
+        bottomPanel.setHgap(10);
+        bottomPanel.setVgap(10);
+        bottomPanel.setPadding(new Insets(20, 0, 0, 0));
+
+        Label sumOfFacesLabel = new Label("Sum of the faces: ");
+        // FIXME
+        sumOfFacesFields = new TextField("25");
+        sumOfFacesFields.setEditable(false);
+        sumOfFacesFields.setPrefWidth(80);
+
+        Label cardsOfHeartsLabel = new Label("Cards of hearts:");
+        // FIXME
+        cardsOfHeartsField = new TextField();
+        cardsOfHeartsField.setEditable(false);
+        cardsOfHeartsField.setPrefWidth(150);
+
+        Label flushLabel = new Label("Flush:");
+        flushField = new TextField("Yes/NO");
+        flushField.setEditable(false);
+        flushField.setPrefWidth(80);
+
+        Label queenOfSpacesLabel = new Label("Queen of spades:");
+        queenOfSpadesFields = new TextField("Yes/NO");
+        queenOfSpadesFields.setEditable(false);
+        queenOfSpadesFields.setPrefWidth(80);
+
+        bottomPanel.add(sumOfFacesLabel, 0, 0);
+        bottomPanel.add(sumOfFacesFields, 1, 0);
+        bottomPanel.add(cardsOfHeartsLabel, 2, 0);
+        bottomPanel.add(cardsOfHeartsField, 3, 0);
+        bottomPanel.add(flushLabel, 0, 1);
+        bottomPanel.add(flushField, 1, 1);
+        bottomPanel.add(queenOfSpacesLabel, 2, 1);
+        bottomPanel.add(queenOfSpadesFields, 3, 1);
+
+        root.setBottom(bottomPanel);
 
         Scene scene = new Scene(root, 700, 500);
         primaryStage.setTitle("Flush Card Game");
@@ -85,7 +123,7 @@ public class JavaFXMain extends Application {
         if (hasFlush) {
             LOGGER.log(Level.INFO, "-------- Flush! ---------");
         }
-        // flushField.setText(hasFlush ? "Yes" : "No");
+        flushField.setText(hasFlush ? "Yes" : "No");
     }
 
     private void dealCards() {
