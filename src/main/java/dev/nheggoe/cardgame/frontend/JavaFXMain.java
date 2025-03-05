79b4198288e79bf5f0546b85f73c489304b1b039
Nick Heggø <nickhe@stud.ntnu.no>
2025-03-05 10:51:58 +0100

    feat: add logging for game actions and enhance UI components

--- src/main/java/dev/nheggoe/cardgame/frontend/JavaFXMain.java
+++ src/main/java/dev/nheggoe/cardgame/frontend/JavaFXMain.java
@@ -4,14 +4,20 @@ import dev.nheggoe.cardgame.backend.engine.CardGameEngine;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Nick Heggø
  * @version 2025.02.26
  */
 public class JavaFXMain extends Application {
+
+    private static final Logger LOGGER = Logger.getLogger(JavaFXMain.class.getName());
 
     public static void main(String[] args) {
         launch();
@@ -22,12 +28,19 @@ public class JavaFXMain extends Application {
         CardGameEngine engine = new CardGameEngine();
         var root = new VBox();
         Button dealHand = new Button("Deal Hand");
-        dealHand.setOnAction(event -> engine.dealHands(1));
+        dealHand.setOnAction(event -> {
+            LOGGER.log(Level.INFO, "Dealing %d cards!".formatted(1));
+            engine.dealHands(1);
+        });
 
         Button checkHand = new Button("Check Hand");
         checkHand.setOnAction(event -> {
+            LOGGER.log(Level.INFO, "Checking hand...");
             if (engine.isFlush()) {
-                // TODO winning scene
+                LOGGER.log(Level.INFO, "-------- Flush! ---------");
+                var root2 = new VBox();
+                root2.getChildren().add(new TilePane());
+
             }
         });
 
